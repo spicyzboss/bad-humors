49691014f397d370cb2f9f0c7c2a6c391e346c69
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-07-26 02:26:35 +0700

    feat: add animejs

--- src/pages/story.tsx
+++ src/pages/story.tsx
@@ -1,10 +1,48 @@
+import { createSignal, Show, onMount } from "solid-js";
 import type { Component } from "solid-js";
+import anime from 'animejs';
 
 const StoryPage: Component = () => {
+  const [time, setTime] = createSignal<number>(0);
+  const [begin, setBegin] = createSignal<boolean>(false);
+  const timeDelay = 6000;
+
+  const beginTime = setInterval(() => {
+    setTime(time()+100);
+    setBegin(Math.floor(time()/timeDelay) > wordlists.length - 1);
+
+    if (begin()) {
+      clearInterval(beginTime);
+    }
+  }, 100);
+
+  const wordlists = [
+    "Nice to meet you.",
+    "How are you today?",
+    "Can you tell me about it?",
+  ];
+
+  onMount(() => {
+    const messageAnimation = anime({
+      targets: '.message',
+      duration: 1500,
+      endDelay: timeDelay/2 - 1500,
+      easing: 'easeInQuint',
+      direction: 'alternate',
+      opacity: '1',
+      loop: wordlists.length * 2,
+      autoplay: false,
+    });
+
+    messageAnimation.play();
+  });
+
   return (
-    <div class="h-screen w-screen flex justify-center items-center">
-      <p>How are you today?</p>
-    </div>
+    <>
+      <div class="h-screen w-screen flex justify-center items-center">
+        <p class="text-8xl opacity-0 font-bold select-none message absolute">{ wordlists[Math.floor(time()/timeDelay)] }</p>
+      </div>
+    </>
   )
 }
 
