51a7998d214e74bcabf73516e8898e647ddb385e
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-07-26 00:44:33 +0700

    feat: add story page

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import type { Component } from 'solid-js';
 import { lazy } from 'solid-js';
 import { Routes, Route } from "solid-app-router";
 const HomePage = lazy(() => import('@pages/index'));
+const StoryPage = lazy(() => import('@pages/story'));
 const NotFoundPage = lazy(() => import('@pages/404'));
 
 const App: Component = () => {
@@ -9,6 +10,7 @@ const App: Component = () => {
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/story' element={<StoryPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ src/pages/story.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const StoryPage: Component = () => {
+  return (
+    <div class="h-screen w-screen flex justify-center items-center">
+      <p>How are you today?</p>
+    </div>
+  )
+}
+
+export default StoryPage;
